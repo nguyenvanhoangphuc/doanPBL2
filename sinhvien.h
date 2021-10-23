2215fb72cbc9ca6e15054e605c5a6bc23f3c2305
toilaphucnguyen <83715016+nguyenvanhoangphuc@users.noreply.github.com>
2021-10-23 15:55:10 +0700

    add file sinhvien.h first

---
+++ sinhvien.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+class Sinhvien {
+    private: 
+        string ma_sv;
+        string tendem; 
+        string ten; 
+        string ngay_sinh;
+        bool gioitinh; 
+        string lop; 
+        string sodth;  
+        string ngay_dk; 
+        string ma_khoa; 
+        string quequan;   
+}; 
